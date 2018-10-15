a59fea95a11ce3720916d4e234bb20201ba442d9
Sunny Patel <sunny_patel33@outlook.com>
2018-10-16 05:05:56 +1100

    Map working properly

---
+++ FIT5032A_1Final/Controllers/ReservationsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FIT5032A_1Final.Models;
+
+namespace FIT5032A_1Final.Controllers
+{
+    public class ReservationsController : Controller
+    {
+        private FIT5032_A1Final db = new FIT5032_A1Final();
+
+        // GET: Reservations
+        public ActionResult Index()
+        {
+            var reservations = db.Reservations.Include(r => r.Employee_Info).Include(r => r.Personal_Info);
+            return View(reservations.ToList());
+        }
+
+        // GET: Reservations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reservation reservation = db.Reservations.Find(id);
+            if (reservation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reservation);
+        }
+
+        // GET: Reservations/Create
+        public ActionResult Create()
+        {
+            ViewBag.EId = new SelectList(db.Employee_Info, "Id", "Fname");
+            ViewBag.PId = new SelectList(db.Personal_Info, "Id", "Fname");
+            return View();
+        }
+
+        // POST: Reservations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "R_Id,R_DateTime,Reason,PId,R_Status,EId")] Reservation reservation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Reservations.Add(reservation);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EId = new SelectList(db.Employee_Info, "Id", "Fname", reservation.EId);
+            ViewBag.PId = new SelectList(db.Personal_Info, "Id", "Fname", reservation.PId);
+            return View(reservation);
+        }
+
+        // GET: Reservations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reservation reservation = db.Reservations.Find(id);
+            if (reservation == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EId = new SelectList(db.Employee_Info, "Id", "Fname", reservation.EId);
+            ViewBag.PId = new SelectList(db.Personal_Info, "Id", "Fname", reservation.PId);
+            return View(reservation);
+        }
+
+        // POST: Reservations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "R_Id,R_DateTime,Reason,PId,R_Status,EId")] Reservation reservation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(reservation).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EId = new SelectList(db.Employee_Info, "Id", "Fname", reservation.EId);
+            ViewBag.PId = new SelectList(db.Personal_Info, "Id", "Fname", reservation.PId);
+            return View(reservation);
+        }
+
+        // GET: Reservations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reservation reservation = db.Reservations.Find(id);
+            if (reservation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reservation);
+        }
+
+        // POST: Reservations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Reservation reservation = db.Reservations.Find(id);
+            db.Reservations.Remove(reservation);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
